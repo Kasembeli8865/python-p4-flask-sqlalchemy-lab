61e7dc742f3ea1568da0300ce3bca87ea62ae23e
Kasembeli8865 <lionel.kasembeli@student.moringaschool.com>
2023-09-20 18:14:06 +0300

    completed assignment

--- server/app.py
+++ server/app.py
@@ -19,16 +19,50 @@ def home():
 
 @app.route('/animal/<int:id>')
 def animal_by_id(id):
-    return ''
+    animal = Animal.query.get(id)  # Use query.get(id) to retrieve an object by its primary key.
+
+    if animal:
+        response_body = f'''
+            <h1>Information for {animal.name}</h1>
+            <h2>Animal Species is {animal.species}</h2>
+            <h2>Animal enclosure is {animal.enclosure_id}</h2>
+            <h2>Zookeeper is {animal.zookeeper_id}</h2>
+        '''
+        response = make_response(response_body, 200)
+    else:
+        response = make_response('<h1>Animal not found</h1>', 404)
+
+    return response
 
 @app.route('/zookeeper/<int:id>')
 def zookeeper_by_id(id):
-    return ''
+    zookeeper = Zookeeper.query.get(id)
+
+    if zookeeper:
+        response_body = f'''
+            <h1>Zookeeper is {zookeeper.name}</h1>
+            <h2>Zookeeper's birthday is {zookeeper.birthday}</h2>
+        '''
+        response = make_response(response_body, 200)
+    else:
+        response = make_response('<h1>Zookeeper not found</h1>', 404)
+
+    return response
 
 @app.route('/enclosure/<int:id>')
 def enclosure_by_id(id):
-    return ''
+    enclosure = Enclosure.query.get(id)
 
+    if enclosure:
+        response_body = f'''
+            <h1>Information for {enclosure.environment}</h1>
+            <h2>Enclosure is open for visitors {enclosure.open_to_visitors}</h2>
+        '''
+        response = make_response(response_body, 200)
+    else:
+        response = make_response('<h1>Enclosure not found</h1>', 404)
+
+    return response
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
--- server/models.py
+++ server/models.py
@@ -11,13 +11,51 @@ class Zookeeper(db.Model):
     __tablename__ = 'zookeepers'
 
     id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    birthday = db.Column(db.Date)
+    
+    # Define a relationship to the Animal model
+    animals = db.relationship('Animal', backref='zookeeper')
+
+    def __init__(self, name, birthday):
+        self.name = name
+        self.birthday = birthday
+
+    def __repr__(self):
+        return f'<Zookeeper {self.name}>'
 
 class Enclosure(db.Model):
     __tablename__ = 'enclosures'
 
     id = db.Column(db.Integer, primary_key=True)
+    environment = db.Column(db.String)  # Grass, Sand, or Water
+    open_to_visitors = db.Column(db.Boolean, default=False)
+    
+    # Define a relationship to the Animal model
+    animals = db.relationship('Animal', backref='enclosure')
+
+    def __init__(self, environment, open_to_visitors=False):
+        self.environment = environment
+        self.open_to_visitors = open_to_visitors
+
+    def __repr__(self):
+        return f'<Enclosure {self.id}: {self.environment}>'
+
 
 class Animal(db.Model):
     __tablename__ = 'animals'
 
     id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    species = db.Column(db.String)
+    zookeeper_id = db.Column(db.Integer, db.ForeignKey('zookeepers.id'))
+    enclosure_id = db.Column(db.Integer, db.ForeignKey('enclosures.id'))
+
+    def __init__(self, name, species, zookeeper_id, enclosure_id):
+        self.name = name
+        self.species = species
+        self.zookeeper_id = zookeeper_id
+        self.enclosure_id = enclosure_id
+
+    def __repr__(self):
+        return f'<Animal {self.name}: {self.species}>'
